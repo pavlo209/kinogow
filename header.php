816470b5114883ba02668c410995ab145bd30665
pavlo209 <pmodsp@gmail.com>
2025-07-27 22:24:45 +0300

    header.php

--- header.php
+++ header.php
@@ -19,7 +19,7 @@ if (!empty($_SESSION['user_id'])) {
 <!-- Bootstrap 5 CSS & JS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="/style.css">
+<link rel="stylesheet" href="style.css">
 
 <!-- Додатковий стиль для повідомлень -->
 <style>
